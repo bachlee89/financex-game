04cc83363934a9634975ed610f4694510b09345f
Bach Lee <bach@balanceinternet.com.au>
2019-09-27 12:11:22 +0700

    Fix s

--- questions.js
+++ questions.js
@@ -3,49 +3,49 @@ const questions = [
         'image': '/images/questions/question-1.jpg',
         'answer': 'mã hóa',
         'suggest': 'm* h**',
-        'explain': 'Cách thức để che giấu một thông tin gì đó'
+        'explain': 'Cách thức để che giấu một thông tin gì đó.'
     },
     {
         'image': '/images/questions/question-2.jpg',
         'answer': 'ẩn danh',
-        'suggest': 'Ẩ* d***',
-        'explain': 'Con số biểu hiện tương đối tình hình biến động của một hiện tượng ở các thời kì khác nhau'
+        'suggest': 'ẩ* d***',
+        'explain': 'Mọi thông tin khách hàng đều được giữ kín, không thể truy tìm'
     },
     {
         'image': '/images/questions/question-3.jpg',
         'answer': 'bảo mật',
         'suggest': 'b** m*t',
-        'explain': 'Có cảm giác bằng lòng, cảm thấy một đòi hỏi nào đó được thoả mãn'
+        'explain': 'Không tiết lộ, mọi thông tin được cất giữ tại nơi an toàn'
     },
     {
         'image': '/images/questions/question-4.jpg',
         'answer': 'thanh khoản',
         'suggest': 't**h k***',
-        'explain': 'Thường dùng để chỉ tinh thần, tâm lí của con người'
+        'explain': 'Chỉ mức độ mà một tài sản bất kì có thể được mua hoặc bán trên thị trường mà không làm ảnh hưởng nhiều đến giá thị trường của tài sản đó'
     },
     {
         'image': '/images/questions/question-5.jpg',
         'answer': 'đông nam á',
         'suggest': 'đ*** n** *',
-        'explain': 'Quá cao, quát đăt, không đúng với thực tế'
+        'explain': 'Là một khu vực nằm ở phía Đông Nam châu Á, bao gồm các nước nằm ở phía nam Trung Quốc'
     },
     {
         'image': '/images/questions/question-6.jpg',
-        'answer': 'khối chuỗi',
-        'suggest': 'k**i c**i',
-        'explain': 'Bị ai đó lấy cái gì đó mà lúc đấy mình không biết'
+        'answer': 'chuỗi khối',
+        'suggest': 'c***i k**i',
+        'explain': 'Công nghệ lưu trữ và truyền tải thông tin bằng các khối được liên kết với nhau và mở rộng theo thời gian'
     },
     {
         'image': '/images/questions/question-7.jpg',
         'answer': 'phân tán',
         'suggest': 'p*** *á*',
-        'explain': 'ở trạng thái vẫn minh mẫn, không để cho tình hình rắc rối, phức tạp tác động đến tư tưởng, tình cảm'
+        'explain': 'Hệ thống phần mềm mà các thành phần cấu tạo nên nó nằm ở trên các máy tính khác nhau được kết nối thành mạng lưới.'
     },
     {
         'image': '/images/questions/question-8.jpg',
         'answer': 'ví lạnh',
         'suggest': 'v* ***h',
-        'explain': 'Dấu hiệu riêng của nơi sản xuất, được dán hoặc in trên mặt hàng'
+        'explain': 'Được sử dụng để lưu trữ Bitcoin ngoại tuyến'
     }
 ];
 
