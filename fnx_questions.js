ef9555351b42394b0f46857ed4f7b4179de9d1f7
Bach Lee <bach@balanceinternet.com.au>
2019-09-28 23:49:36 +0700

    Fix css

--- fnx_questions.js
+++ fnx_questions.js
@@ -1,33 +1,34 @@
 const fnx_questions = [
+
     {
-        'question': 'Ai là người đã tạo ra Bitcoin?',
+        'question': 'Ai là người sáng lập Ethereum?',
         'options': {
             1: 'Gavin Andresen',
             2: 'Satoshi Nakamoto',
             3: 'Dorian Nakamoto',
-            4: 'Paul Krugman',
-        },
-        'answer': 2
-    },
-    {
-        'question': 'Bitcoin đã trải qua bao nhiêu năm thăng trầm?',
-        'options': {
-            1: '5 năm',
-            2: '10 năm',
-            3: '15 năm',
-            4: '20 năm',
-        },
-        'answer': 2
-    },
-    {
-        'question': 'Những sản phẩm và dịch vụ của FinanceX là gì?',
-        'options': {
-            1: 'Liên sàn giao dịch hỗ trợ Fiat',
-            2: 'Dịch vụ và giải pháp phần mềm cho doanh nghiệp',
-            3: 'Đào tạo Labs và Blockchain Dapp',
-            4: 'Tất cả các phương án trên',
+            4: 'Vitalik Buterin',
         },
         'answer': 4
+    },
+    {
+        'question': 'IEO là gì?',
+        'options': {
+            1: 'Information Exchange Offering',
+            2: 'Imitation Exchange Offering',
+            3: 'Initial Exchange Offering',
+            4: 'Imitation Exchange Offering',
+        },
+        'answer': 3
+    },
+    {
+        'question': 'Nơi lưu trữ tiền mã hóa an toàn nhất?',
+        'options': {
+            1: 'Ví nóng',
+            2: 'Ví lạnh',
+            3: 'Sàn giao dịch',
+            4: 'Tất cả các phương án trên'
+        },
+        'answer': 1
     },
 ];
 
